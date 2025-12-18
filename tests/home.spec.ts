4f5c3e6d378449f78de58af0bc81f66f658158fd
gabrielsouza80 <gabriel.dias808005@gmail.com>
2025-12-18 16:28:28 +0000

    mudando comentarios de portugues para ingles

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -10,31 +10,31 @@ test.describe('Home Page', () => {
   // BEFORE EACH: Executed before each test — browse and accept cookies
   test.beforeEach(async ({ page }) => {
     const pm = new PageManager(page);
-    await pm.onHomePage().navigateAndAcceptCookies(); // Navega e aceita cookies via PageManager
+    await pm.onHomePage().navigateAndAcceptCookies(); // Browse and accept cookies via PageManager.
   });
 
 
   // TC 1 — Validate Iglu Ski Logo: checks for redirection when clicking on the logo
-  test('Validar Logo da Iglu Ski', async ({ page }) => {
+  test('Validate Iglu Ski Logo', async ({ page }) => {
     const pm = new PageManager(page);
     await pm.onHomePage().validateLogo();
   });
 
   // TC 2 — Validate Menu + Submenus: validates menus, sublinks, and <h1>; resumes duplicates
-  test('Validar Menu de Navegação Principal', async ({ page }) => {
-    // ✅ Aumenta timeout global para este teste (5 minutos)
+  test('Validate Main Navigation Menu', async ({ page }) => {
+    // ✅ Increase the global timeout for this test (5 minutes)
     test.setTimeout(300000);
 
     const pm = new PageManager(page);
 
-    // ✅ Valida todos os sublinks (ilimitado)
+    // ✅ Validates all sublinks (unlimited)
     await pm.onHomePage().validateMenuAndSubMenuNavigation();
 
   });
 
-  test('Clicar no menu e opcionalmente no submenu', async ({ page }) => {
+  test('Click on the menu and optionally on the submenu.', async ({ page }) => {
     const pm = new PageManager(page);
-    
+
     // ✅ Just click on the main menu:
     await pm.onHomePage().clickMenu("Ski Holidays");
 
@@ -43,7 +43,7 @@ test.describe('Home Page', () => {
   });
 
   // TC 3 — Validate Contact Information in Header: checks phone number and email address
-  test('Validar Informações de Contato no Header', async ({ page }) => {
+  test('Validate Contact Information in the Header', async ({ page }) => {
     const pm = new PageManager(page);
 
     await pm.onHomePage().validateHeaderContactInfo();
@@ -51,19 +51,20 @@ test.describe('Home Page', () => {
   });
 
   //TC 4 — Validate "Recently Viewed" Button
-  test('Validar Botão "Recently Viewed"', async ({ page }) => {
+  test('Validate "Recently Viewed" button"', async ({ page }) => {
     const pm = new PageManager(page);
     await pm.onHomePage().validateRecentlyViewedButton();
   });
 
   //TC 16 — Validate carousel of promotions and country banners on the homepage"
-  test('Validar Carrossel de Banners (Países)"', async ({ page }) => {
+  test.skip('Validate Carousel of Banners (Countries)"', async ({ page }) => {
     const pm = new PageManager(page);
     await pm.onHomePage().validateCarouselHome();
-  }); 
+    await pm.onHomePage().validateBannersHome();
+  });
 
   //TC 20 — Validate Footer Links
-  test('Validar Links do Footer"', async ({ page }) => {
+  test('Validate Footer Links"', async ({ page }) => {
     // ✅ Increase the global timeout for this test (5 minutes)
     test.setTimeout(300000);
     const pm = new PageManager(page);
